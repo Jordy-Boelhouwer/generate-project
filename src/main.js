cf1e0c39cce8e211021e7680fdf9a94955f177e5
Jordy Boelhouwer <jordyboelhouwer@hotmail.com>
2021-03-04 11:02:42 +0100

    feature: add option to initialize git and install dependencies

--- src/main.js
+++ src/main.js
@@ -3,42 +3,76 @@ import fs from 'fs';
 import ncp from 'ncp';
 import path from 'path';
 import { promisify } from 'util';
+import execa from 'execa';
+import Listr from 'listr';
+import { projectInstall } from 'pkg-install';
 
 const access = promisify(fs.access);
 const copy = promisify(ncp);
 
 async function copyTemplateFiles(options) {
- return copy(options.templateDirectory, options.targetDirectory, {
-   clobber: false,
- });
+  return copy(options.templateDirectory, options.targetDirectory, {
+    clobber: false,
+  });
+}
+
+async function initGit(options) {
+  const result = await execa('git', ['init'], {
+    cwd: options.targetDirectory,
+  });
+  if (result.failed) {
+    return Promise.reject(new Error('Failed to initialize git'));
+  }
+  return;
 }
 
 export async function createProject(options) {
- options = {
-   ...options,
-   targetDirectory: options.targetDirectory || process.cwd(),
- };
+  options = {
+    ...options,
+    targetDirectory: options.targetDirectory || process.cwd(),
+  };
 
- const currentFileUrl = import.meta.url;
- console.log(__dirname)
- const templateDir = path.resolve(
-   __dirname,
-   '../templates',
-   options.template.toLowerCase()
- );
- console.log(`template dir is ${templateDir}`);
- options.templateDirectory = templateDir;
+  const currentFileUrl = import.meta.url;
+  console.log(__dirname);
+  const templateDir = path.resolve(
+    __dirname,
+    '../templates',
+    options.template.toLowerCase()
+  );
+  console.log(`template dir is ${templateDir}`);
+  options.templateDirectory = templateDir;
 
- try {
-   await access(templateDir, fs.constants.R_OK);
- } catch (err) {
-   console.error(err, chalk.red.bold('ERROR'));
-   process.exit(1);
- }
+  try {
+    await access(templateDir, fs.constants.R_OK);
+  } catch (err) {
+    console.error(err, chalk.red.bold('ERROR'));
+    process.exit(1);
+  }
 
- console.log('Copy project files');
- await copyTemplateFiles(options);
+  const tasks = new Listr([
+    {
+      title: 'Copy project files',
+      task: () => copyTemplateFiles(options),
+    },
+    {
+      title: 'Initialize git',
+      task: () => initGit(options),
+      enabled: () => options.git,
+    },
+    {
+      title: 'Install dependencies',
+      task: () =>
+        projectInstall({
+          cwd: options.targetDirectory,
+        }),
+      skip: () =>
+        !options.runInstall
+          ? 'Pass --install to automatically install dependencies'
+          : undefined,
+    },
+  ]);
 
- console.log('%s Project ready', chalk.green.bold('DONE'));
- return true;
+  await tasks.run();
+  console.log('%s Project ready', chalk.green.bold('DONE'));
+  return true;
 }
